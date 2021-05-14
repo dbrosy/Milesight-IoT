1cf06519a97e79a790f9c96e06b19517b4f13f81
dbrosy <dbrosy@hotmail.com>
2021-05-14 20:35:26 +1200

    Update EM300-TH_TTN_V3.js

--- SensorDecoders/EM300_Series/EM300-TH/EM300-TH_TTN_V3.js
+++ SensorDecoders/EM300_Series/EM300-TH/EM300-TH_TTN_V3.js
@@ -26,7 +26,7 @@
         // TEMPERATURE
         else if (channel_id === 0x03 && channel_type === 0x67) {
             // ℃
-            data.temperature = readInt16LE(bytes.slice(i, i + 2)) / 10;
+            data.temperature = readInt16LE(input.bytes.slice(i, i + 2)) / 10;
             i += 2;
 
             // ℉
